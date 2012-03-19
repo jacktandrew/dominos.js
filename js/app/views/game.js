61d6b1b0c06ab50f836b3d0948d0132cdfd828f1
Jack Andrew <jacktandrew@gmail.com>
2012-03-19 16:51:47 -0700

    add new files

---
+++ js/app/views/game.js
@@ -0,0 +1,15 @@
+window.views = window.views || {}
+views.Game = Backbone.View.extend({
+  
+  initialize: function() {
+    _.bindAll(this)
+  },
+
+  events: {
+  },
+
+  render: function() {
+    
+  },
+  
+});
